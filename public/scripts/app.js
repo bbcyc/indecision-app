7231f6493def7fb66f15fcb61c41178d36e2c69c
Brian Berry <datildoyah@bitbucket.org>
2018-08-02 07:56:07 -0400

    title

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,69 @@
-console.log('App.js is running')
+"use strict";
+
+console.log('App.js is running');
 
 // JSX - JavaScript XML
-//var template = <p>This is JSX from app.js!</p>;
+
+var page = {
+  title: "Brian's App",
+  subtitle: "A great place to visit"
+};
 var template = React.createElement(
-  "p",
+  "div",
   null,
-  "This is JSX from app.js!"
+  React.createElement(
+    "h1",
+    null,
+    page.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    page.subtitle
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
 );
+
+var user = {
+  name: 'Andrew',
+  age: 26,
+  location: 'Philadelphia'
+};
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    user.location
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
