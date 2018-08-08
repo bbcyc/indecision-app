670209df1d4422a9c42c1bcd725d089b52e5c54f
Brian Berry <datildoyah@bitbucket.org>
2018-08-08 07:14:37 -0400

    buttons

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -2,3 +2,12 @@ var nameVar = 'Andrew';
 var nameVar = 'Mike';
 console.log('nameVar', nameVar);
 
+const multiplier = {
+  numbers: [1, 2, 3],
+  multiplyBy: 4,
+  multiply() {
+    return this.numbers.map((number) => number * this.multiplyBy)
+  } 
+}
+
+console.log(multiplier.multiply());
