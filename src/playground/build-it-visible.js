44cc2c16a34f76f29ee2b056c62ead841948b11a
Brian Berry <datildoyah@bitbucket.org>
2018-08-22 06:52:45 -0400

    webpack

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,4 +1,39 @@
-const app = {
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+    this.state = {
+      visibility: false 
+    }
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? 'Hide Details' : 'Show Details'}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Here are the details</p>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
+
+
+/* const app = {
   title: "Visibility Toggle",
   showDetails: true
 }
@@ -30,4 +65,4 @@ const render = () => {
   ReactDOM.render(template, appRoot)
 }
 
-render()
+render() */
